5a8342df489d1cccca427e68a1e6e5471586ce19
Humberto Filho <humbertofilho5@gmail.com>
2020-09-15 21:26:58 -0400

    removendo coisas desnecessárias

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/global";
-import { ToastContainer } from "react-toastify";
 import Routes from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes />
-      <ToastContainer autoClose={3000} />
       <GlobalStyle />
     </BrowserRouter>
   );
